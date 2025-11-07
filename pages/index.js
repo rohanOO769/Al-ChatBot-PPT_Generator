65dcd7602bbe43c6a33a5638b113f17a0b8bc84f
rohanOO769 <aminrohan54@gmail.com>
2025-11-07 21:49:46 +0530

    bug fix - UI

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="text-3xl font-bold text-red-500 underline">
-          Hello world!
-        </h1>
-
         <section style={{ width: "100%", marginTop: 24 }}>
           <ChatSlideBuilder userName="Rohan" />
         </section>
